3ccf3c3b0391e9fdbcc008eeaee834e02e304ad4
Uzias Lara <neon-zap-divisible@duck.com>
2026-03-24 15:51:18 -0400

    feat(lvl-07): solve The Four Sisters and the Duckbear Challenge

---
+++ Level07_Math/TheFourSistersAndTheDuckbear/Program.cs
@@ -0,0 +1,13 @@
+﻿// Write a program that takes a number of eggs as input and equally divide that number among the four sisters from the book.
+// Display the remaining number of eggs that their duckbear pet should receive.
+// Disclaimer: For simplicity, this code has no checks.
+
+Console.Write("How many eggs were laid today? " );
+int numberOfEggsLaid = Convert.ToInt32(Console.ReadLine());
+
+int eggsPerSister = numberOfEggsLaid / 4;
+int eggsForDuckbear = numberOfEggsLaid % 4;
+
+Console.WriteLine($"Number of Eggs entered: {numberOfEggsLaid}");
+Console.WriteLine($"Each sister is entitled to {eggsPerSister} egg(s) today.");
+Console.WriteLine($"Their Duckbear will receive {eggsForDuckbear} egg(s) this time.");
---
+++ Level07_Math/TheFourSistersAndTheDuckbear/TheFourSistersAndTheDuckbear.md
@@ -0,0 +1,5 @@
+## The Four Sisters and the Duckbear
+
+**Give at least 3 cases in which the Duckbear will get more eggs than each sister.**
+
+> The Duckbear will receive more eggs than each sister when **1**,**2**, or **3** eggs are laid.
