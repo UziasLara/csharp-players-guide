d197986e72b4b53f7d8c7baba66ed55e099a6ef3
Uzias Lara <neon-zap-divisible@duck.com>
2026-03-24 13:54:18 -0400

    feat(lvl-06): solve The Variable Shop Returns Challenge

---
+++ Level06_TheCSharpTypeSystem/TheVariableShopReturns/Program.cs
@@ -0,0 +1,60 @@
+﻿// Create a program displaying all 14 variable types described in this level and display them.
+// Update the program with new values for each variable.
+
+sbyte minSByte = -128;
+sbyte maxSByte = 127;
+
+byte minByte = -0;
+byte maxByte = 255;
+
+short minShort = -32768;
+short maxShort = 32767;
+
+ushort minUShort = 0;
+ushort maxUShort = 65535;
+
+int minInt = -2147483648;
+int maxInt = 2147483647;
+
+uint minUInt = 0;
+uint maxUInt = 4294967295;
+
+long minLong = -9223372036854775808;
+long maxLong = 9223372036854775807;
+
+ulong minULong = 0;
+ulong maxULong = 18446744073709551615;
+
+float minFloat = -3.4028235E+38f;
+float maxFloat = 3.4028235E+38f;
+
+double minDouble = -1.7976931348623157E+308;
+double maxDouble = 1.7976931348623157E+308;
+
+decimal minDecimal = -79228162514264337593543950335m;
+decimal maxDecimal = 79228162514264337593543950335m;
+
+string firstName = "John";
+string lastName = "Doe";
+char firtNameInitial = 'J';
+char lastNameInitial = 'D';
+
+bool isMyName = false;
+
+// Console.WriteLine($"type ranges from {type.MinValue} to {type.MaxValue}");
+// could also be used to avoid writing out variable names but it would not satisfy the requirements.
+
+Console.WriteLine($"sbyte ranges from: {minSByte} to {maxSByte}");
+Console.WriteLine($"byte ranges from: {minByte} to {maxByte}");
+Console.WriteLine($"short ranges from: {minShort} to {maxShort}");
+Console.WriteLine($"ushort ranges from: {minUShort} to {maxUShort}");
+Console.WriteLine($"int ranges from: {minInt} to {maxInt}");
+Console.WriteLine($"uint ranges from: {minUInt} to {maxUInt}");
+Console.WriteLine($"long ranges from: {minLong} to {maxLong}");
+Console.WriteLine($"ulong ranges from: {minULong} to {maxULong}");
+Console.WriteLine($"float ranges from: {minFloat} to {maxFloat}");
+Console.WriteLine($"double ranges from: {minDouble} to {maxDouble}");
+Console.WriteLine($"decimal ranges from: {minDecimal} to {maxDecimal}");
+Console.WriteLine();
+Console.WriteLine($"Default name is {firstName} {lastName} with initials as {firtNameInitial}. {lastNameInitial}.");
+Console.WriteLine($"Is that my name? {isMyName}");
--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 ![C#](https://custom-icon-badges.demolab.com/badge/C%23-%2300599C.svg?logo=cshrp&logoColor=white)
 ![Current Level](https://custom-icon-badges.demolab.com/badge/Level-3-01A4FF.svg?logo=shield-check&logoColor=white) 
-![Total XP](https://custom-icon-badges.demolab.com/badge/Total%20XP-525-A7F175.svg?logo=bookmark-fill&logoColor=white)
-![Next Level](https://custom-icon-badges.demolab.com/badge/XP%20to%20Level%204-75-0052CC.svg?logo=rocket&logoColor=white)
-![Progress](https://custom-icon-badges.demolab.com/badge/Progress-9%20%2F%20113-F4AA41.svg?logo=star-fill&logoColor=white)
+![Total XP](https://custom-icon-badges.demolab.com/badge/Total%20XP-575-A7F175.svg?logo=bookmark-fill&logoColor=white)
+![Next Level](https://custom-icon-badges.demolab.com/badge/XP%20to%20Level%204-25-0052CC.svg?logo=rocket&logoColor=white)
+![Progress](https://custom-icon-badges.demolab.com/badge/Progress-10%20%2F%20113-F4AA41.svg?logo=star-fill&logoColor=white)
 
 I endeavor to showcase my progress throughout the book [*The C# Player's Guide*](https://csharpplayersguide.com/) by RB Whitaker.
 
@@ -49,7 +49,7 @@ I will also attempt to link every completed challenge to its respective solution
 
 #### Level 6 - The C# Type System
 - [X] [Challenge: The Variable Shop](./Level06_TheCSharpTypeSystem/TheVariableShop/) `+100 XP`
-- [ ] Challenge: The Variable Shop Returns `+50 XP`
+- [X] [Challenge: The Variable Shop Returns](./Level06_TheCSharpTypeSystem/TheVariableShopReturns/) `+50 XP`
 - [ ] Knowledge Check: Type System `+25 XP`
 
 #### Level 7 - Math
