36c4023a99153c58d8ae2eaf81f567330ae8871d
Uzias Lara <neon-zap-divisible@duck.com>
2026-03-25 17:36:12 -0400

    feat(lvl-12): solve The Laws of Freach challenge

---
+++ Part01_TheBasics/Level12_Arrays/TheLawsOfFreach/Program.cs
@@ -0,0 +1,20 @@
+﻿// Using foreach loops, compute the average and smallest value in the provided array: { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+
+int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+int currentSmallest = int.MaxValue; // Begin with the highest possible int
+
+foreach(int value in array)
+{
+    if(value <= currentSmallest) currentSmallest = value;
+}
+Console.WriteLine(currentSmallest);
+
+int total = 0;
+
+foreach(int value in array)
+{
+    total += value;
+}
+
+float average = (float)total / array.Length;
+Console.WriteLine(average);
