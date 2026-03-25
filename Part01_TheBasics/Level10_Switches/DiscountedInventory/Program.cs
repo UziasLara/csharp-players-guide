a0897481a681d260b3903e07ff7d13a2ba118d95
Uzias Lara <neon-zap-divisible@duck.com>
2026-03-25 14:38:17 -0400

    feat(lvl-10): solve Discounted Inventory challenge

---
+++ Part01_TheBasics/Level10_Switches/DiscountedInventory/Program.cs
@@ -0,0 +1,57 @@
+﻿// Modify the program to discount the given price by 50% if the name input matches yours.
+// Write a program that displays a list of items.
+// Prompt the user to choose an item from the list and display its respective price.
+// Disclaimer: For simplicity, this code has no input checks.
+
+Console.Write("Welcome to Tortuga's Shop!\nWho are we serving today?: ");
+string name = Console.ReadLine();
+float discount = 0.50f;
+
+Console.WriteLine();
+Console.WriteLine($"Hello {name}, the following items are available:");
+Console.WriteLine("----------------------------------------------");
+Console.WriteLine("1 -- Rope");
+Console.WriteLine("2 -- Torches");
+Console.WriteLine("3 -- Climbing Equipment");
+Console.WriteLine("4 -- Clean Water");
+Console.WriteLine("5 -- Machete");
+Console.WriteLine("6 -- Canoe");
+Console.WriteLine("7 -- Food Supplies");
+Console.WriteLine("----------------------------------------------");
+
+Console.Write("What number do you want to see the price of? ");
+int userInput = Convert.ToInt32(Console.ReadLine());
+
+string item = userInput switch
+{
+    1 => "Rope",
+    2 => "Torches",
+    3 => "Climbing Equipment",
+    4 => "Clean Water",
+    5 => "Machete",
+    6 => "Canoe",
+    7 => "Food Supplies",
+    _ => "NotAnItem"
+};
+
+float price = item switch
+{
+    "Rope" => 10,
+    "Torches" => 15,
+    "Climbing Equipment" => 25,
+    "Clean Water" => 1,
+    "Machete" => 20,
+    "Canoe" => 200,
+    "Food Supplies" => 1,
+    _ => 999_999_999
+};
+
+if (name == "Uzias") price *= discount;
+if (item != "NotAnItem")
+{
+    Console.WriteLine($"{item} costs {price} gold.");
+}
+else
+{
+    Console.WriteLine("That is not a valid selection.");
+}
--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 ![C#](https://custom-icon-badges.demolab.com/badge/C%23-%2300599C.svg?logo=cshrp&logoColor=white)
 ![Current Level](https://custom-icon-badges.demolab.com/badge/Level-8-01A4FF.svg?logo=shield-check&logoColor=white) 
-![Total XP](https://custom-icon-badges.demolab.com/badge/Total%20XP-1400-A7F175.svg?logo=bookmark-fill&logoColor=white)
-![Next Level](https://custom-icon-badges.demolab.com/badge/XP%20to%20Level%209-200-0052CC.svg?logo=rocket&logoColor=white)
-![Progress](https://custom-icon-badges.demolab.com/badge/Progress-18%20%2F%20113-F4AA41.svg?logo=star-fill&logoColor=white)
+![Total XP](https://custom-icon-badges.demolab.com/badge/Total%20XP-1450-A7F175.svg?logo=bookmark-fill&logoColor=white)
+![Next Level](https://custom-icon-badges.demolab.com/badge/XP%20to%20Level%209-150-0052CC.svg?logo=rocket&logoColor=white)
+![Progress](https://custom-icon-badges.demolab.com/badge/Progress-19%20%2F%20113-F4AA41.svg?logo=star-fill&logoColor=white)
 
 I endeavor to showcase my progress throughout the book [*The C# Player's Guide*](https://csharpplayersguide.com/) by RB Whitaker.
 
@@ -66,7 +66,7 @@ I will also attempt to link every completed challenge to its respective solution
 
 #### Level 10 - Switches
 - [X] [Challenge: Buying Inventory](./Part01_TheBasics/Level10_Switches/BuyingInventory/) `+100 XP`
-- [ ] Challenge: Discounted Inventory `+50 XP`
+- [X] [Challenge: Discounted Inventory](./Part01_TheBasics/Level10_Switches/DiscountedInventory/) `+50 XP`
 
 #### Level 11 - Looping
 - [ ] Challenge: The Prototype `+100 XP`
